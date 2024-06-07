19bd794fe3697555c2fbbc34ba7ee8037f4b6858
camilazci <camilazucchi@outlook.com>
2024-06-07 15:03:36 -0300

    refactor: alter variable name from template to TEMPLATE to fix sonarlint issue

--- java-spring-rest/src/main/java/br/com/zucchicamila/java_spring_rest/GreetingController.java
+++ java-spring-rest/src/main/java/br/com/zucchicamila/java_spring_rest/GreetingController.java
@@ -9,13 +9,13 @@ import java.util.concurrent.atomic.AtomicLong;
 @RestController
 public class GreetingController {
 
-    private static final String template = "Hello, %s!";
+    private static final String TEMPLATE = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     // Método responsável por devolver o endpoint Greeting
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "world") String name) {
-        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+        return new Greeting(counter.incrementAndGet(), String.format(TEMPLATE, name));
     }
 
 }
