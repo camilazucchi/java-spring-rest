87317999a511477f53e33720f1bea77d17774d56
camilazci <camilazucchi@outlook.com>
2024-06-07 14:59:54 -0300

    feat: add greeting endpoint example implementation

---
+++ java-spring-rest/src/main/java/br/com/zucchicamila/java_spring_rest/Greeting.java
@@ -0,0 +1,21 @@
+package br.com.zucchicamila.java_spring_rest;
+
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
