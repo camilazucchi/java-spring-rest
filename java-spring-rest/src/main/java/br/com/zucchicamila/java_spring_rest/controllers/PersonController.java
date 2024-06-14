5413246c2eca366b25cbf3a56a43c6c181308c47
camilazci <camilazucchi@outlook.com>
2024-06-14 13:41:54 -0300

    feat: add findById method to PersonController

---
+++ java-spring-rest/src/main/java/br/com/zucchicamila/java_spring_rest/controllers/PersonController.java
@@ -0,0 +1,27 @@
+package br.com.zucchicamila.java_spring_rest.controllers;
+
+import br.com.zucchicamila.java_spring_rest.models.Person;
+import br.com.zucchicamila.java_spring_rest.services.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+
+    @Autowired
+    private PersonService service;
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person findById(@PathVariable(value = "id") Long id) {
+        return service.findById(id);
+    }
+
+
+
+
+}
