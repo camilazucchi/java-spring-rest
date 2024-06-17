b6784ab07d0547714aeb7bdc4833a6654bf195c8
camilazci <camilazucchi@outlook.com>
2024-06-17 15:19:51 -0300

    refactor: remove repository annotation

--- java-spring-rest/src/main/java/br/com/zucchicamila/java_spring_rest/repositories/PersonRepository.java
+++ java-spring-rest/src/main/java/br/com/zucchicamila/java_spring_rest/repositories/PersonRepository.java
@@ -2,7 +2,5 @@ package br.com.zucchicamila.java_spring_rest.repositories;
 
 import br.com.zucchicamila.java_spring_rest.models.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {}
