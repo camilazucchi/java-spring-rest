8e9cfde22e36913718d04a941651b233bfa07540
camilazci <camilazucchi@outlook.com>
2024-07-02 15:21:57 -0300

    feat: add migrations V1 and V2 (create and populate table Person through migrations)

---
+++ java-spring-rest/src/main/resources/db/migration/V1__Create_Table_Person.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Person (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    address VARCHAR(255),
+    gender VARCHAR(10)
+);
---
+++ java-spring-rest/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,8 @@
+INSERT INTO Person (first_name, last_name, address, gender)
+VALUES
+('John', 'Doe', '123 Main St', 'Male'),
+('Jane', 'Smith', '456 Elm St', 'Female'),
+('Alex', 'Johnson', '789 Oak St', 'Non-Binary'),
+('Maria', 'Gonzalez', '101 Maple Ave', 'Female'),
+('Carlos', 'Ramirez', '202 Pine St', 'Male'),
+('Samantha', 'Brown', '303 Birch Blvd', 'Female');
