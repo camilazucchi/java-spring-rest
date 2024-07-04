d90684ae97007892580359fcd72b0bc367ac8b7c
camilazci <camilazucchi@outlook.com>
2024-07-04 14:02:36 -0300

    feat: add extendMessageConverters method to WebConfig class

--- java-spring-rest/src/main/java/br/com/zucchicamila/java_spring_rest/config/WebConfig.java
+++ java-spring-rest/src/main/java/br/com/zucchicamila/java_spring_rest/config/WebConfig.java
@@ -1,15 +1,26 @@
 package br.com.zucchicamila.java_spring_rest.config;
 
+import br.com.zucchicamila.java_spring_rest.serialization.converter.YamlJackson2HttpMessageConverter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 /* A anotação "@Configuration" é usada para marcar uma classe como uma fonte de configurações do Spring. Podemos pensar
  * nela como uma forma de dizer ao Spring que a classe contém definições de como os objetos (beans) devem ser criados
  * e configurados.  */
 public class WebConfig implements WebMvcConfigurer {
+
+    private static final MediaType MEDIA_TYPE_APPLICATION_YAML = MediaType.valueOf("application/x-yaml");
+
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(new YamlJackson2HttpMessageConverter());
+    }
 
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
@@ -28,7 +39,8 @@ public class WebConfig implements WebMvcConfigurer {
                     .useRegisteredExtensionsOnly(false)
                     .defaultContentType(MediaType.APPLICATION_JSON)
                     .mediaType("json", MediaType.APPLICATION_JSON)
-                    .mediaType("xml", MediaType.APPLICATION_XML);
+                    .mediaType("xml", MediaType.APPLICATION_XML)
+                    .mediaType("x-yaml", MEDIA_TYPE_APPLICATION_YAML);
     }
 
 }
